01deb0a0f21067c766e165403b17312a7388d1ae
Valeriya Litvinchuk <43747693+vvlit@users.noreply.github.com>
2023-09-20 19:22:51 +0200

    8.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,24 @@
 import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
+player.on('timeupdate', throttle(saveTimeToLocalStorage, 1000));
+
+
+function saveTimeToLocalStorage (timeupdateDataObject) {
+    localStorage.setItem("videoplayer-current-time", timeupdateDataObject.seconds);
+};
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+          break;
+        default:
+            break;
+    }
+});
 
 
