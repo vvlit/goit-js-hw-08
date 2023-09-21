6bca96ff8a950e0de69cf4a95613464a623683ab
Valeriya Litvinchuk <43747693+vvlit@users.noreply.github.com>
2023-09-21 15:24:43 +0200

    8.3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from "lodash.throttle";
+
+const form = document.querySelector(".feedback-form");
+const localStorageKey = "feedback-form-state";
+const localStorageData = localStorage.getItem(localStorageKey);
+const formAutoFillData = JSON.parse(localStorageData);
+
+if (localStorageData) {
+    form.elements.email.value = formAutoFillData.email;
+    form.elements.message.value = formAutoFillData.message;
+};
+
+form.addEventListener("input", throttle(saveInputDataToLocalStorage, 500));
+form.addEventListener("submit", clearFormAndStorage);
+
+function saveInputDataToLocalStorage(event) {
+    inputData = {
+        email: form.elements.email.value,
+        message: form.elements.message.value
+    };
+
+    const localStorageData = localStorage.setItem(localStorageKey, JSON.stringify(inputData));
+};
+
+function clearFormAndStorage (event) {
+  event.preventDefault();
+    console.log({
+        email: form.elements.email.value,
+        message: form.elements.message.value
+    });
+    localStorage.removeItem(localStorageKey);
+    form.reset();
+};
+
 
